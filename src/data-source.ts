06b993d8d70f164f05bf86d8e3d851b0c402f7d4
Rodrigo Cepeda <rodrigo.cepeda.1993@gmail.com>
2025-09-17 15:50:42 -0300

    Options for Neon DB datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,6 +4,12 @@ import * as env from 'dotenv';
 
 env.config();
 
+const host = process.env.DB_HOST;
+const username = process.env.DB_USERNAME;
+const password = process.env.DB_PASSWORD;
+const database = process.env.DB_NAME;
+const databaseOptions = process.env.DB_OPTIONS;
+
 const sqliteDataSource = new DataSource({
   type: 'sqlite',
   database: 'local.sqlite',
@@ -11,13 +17,11 @@ const sqliteDataSource = new DataSource({
   migrations: [__dirname + '/migrations/dev/*{.js,.ts}'],
 } as DataSourceOptions);
 
+const connectionUrl = `postgresql://${username}:${password}@${host}:${process.env.DB_PORT}/${database}?${databaseOptions}`;
+
 const postgresDataSource = new DataSource({
-  type: process.env.DB_TYPE,
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  type: 'postgres',
+  url: connectionUrl,
   entities: ['**/*.entity*{.js,.ts}'],
   migrations: [__dirname + '/migrations/prod/*{.js,.ts}'],
 } as DataSourceOptions);
